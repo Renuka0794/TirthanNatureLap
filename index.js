843dda4bbcaca6ea10150f23bae0dcbb06597d04
Renuka <renukas462@gmail.com>
2021-02-20 15:23:07 +0530

    Added all project files

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".banner-wrapper").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+
+  $("#places-arround-us .page-block").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  });
+});
